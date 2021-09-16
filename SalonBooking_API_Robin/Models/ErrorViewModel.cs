446f701aa7550bca34456fba5f217d9bb4bf9f93
Robinpreet3374@gmail.com <Robinpreet3374@gmail.com>
2021-09-16 12:50:16 +0300

    Add project files.

---
+++ SalonBooking_API_Robin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalonBooking_API_Robin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
